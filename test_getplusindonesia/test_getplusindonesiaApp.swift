fc44c54231d493465773e5cd2db58143deda9dcd
galesaputra <revolveheld@gmail.com>
2023-02-02 18:37:12 +0700

    Initial Commit

---
+++ test_getplusindonesia/test_getplusindonesiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_getplusindonesiaApp.swift
+//  test_getplusindonesia
+//
+//  Created by Gale on 02/02/23.
+//
+
+import SwiftUI
+
+@main
+struct test_getplusindonesiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
